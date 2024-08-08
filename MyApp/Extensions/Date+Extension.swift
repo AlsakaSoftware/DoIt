f00d1b91fa16588506395674da16d9e949612cbf
karimalsaka <karimalsaka@hotmail.com>
2024-08-08 14:21:04 +0100

    improve title date format on list view

--- MyApp/Extensions/Date+Extension.swift
+++ MyApp/Extensions/Date+Extension.swift
@@ -25,10 +25,26 @@ extension Date {
         } else if calendar.isDateInTomorrow(self) {
             return "Tomorrow"
         } else {
-            return dateStringDisplayFormat()
+            return dayAndMonthListTitleFormat()
         }
     }
-    
+        
+    func dayAndMonthListTitleFormat() -> String {
+        let day = Calendar.current.component(.day, from: self)
+        
+        // Handle ordinal suffix
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .ordinal
+        let dayString = numberFormatter.string(from: NSNumber(value: day))!
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM"
+        let monthString = dateFormatter.string(from: self)
+        
+        return "\(monthString) \(dayString)"
+    }
+
+    // Not sure if i need this, might delete
     func dateStringDisplayFormat() -> String {
         let calendar = Calendar.current
 
@@ -45,4 +61,5 @@ extension Date {
 
         return dateFormatter.string(from: self)
     }
+
 }
