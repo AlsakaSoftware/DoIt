245586ca346d25cfa5a265a00cc444c8fae825fc
karimalsaka <karimalsaka@hotmail.com>
2024-08-11 12:39:48 +0100

    move ToDoListDropViewDelegate to its own file

--- MyApp/Home/Views/HomeView.swift
+++ MyApp/Home/Views/HomeView.swift
@@ -202,7 +202,7 @@ struct HomeView: View {
             return NSItemProvider()
         }
         .onDrop(of: [.toDoItem],
-                delegate: DropViewDelegate(destinationItem: item, items: $viewModel.todaysList.items, draggedItem: $draggedItem)
+                delegate: ToDoListDropViewDelegate(destinationItem: item, items: $viewModel.todaysList.items, draggedItem: $draggedItem)
         )
     }
     
@@ -245,35 +245,4 @@ struct HomeView: View {
     let viewModel = HomeViewModel(authManager: AuthManager(), databaseManager: DatabaseManager())
     return HomeView(viewModel: viewModel)
         .environmentObject(HomeFlowCoordinator<HomeFlowRouter>(authManager: AuthManager(), databaseManager: DatabaseManager()))
-}
-
-
-struct DropViewDelegate: DropDelegate {
-    
-    let destinationItem: ToDoItem
-    @Binding var items: [ToDoItem]
-    @Binding var draggedItem: ToDoItem?
-    
-    func dropUpdated(info: DropInfo) -> DropProposal? {
-        return DropProposal(operation: .move)
-    }
-    
-    func performDrop(info: DropInfo) -> Bool {
-        draggedItem = nil
-        return true
-    }
-    
-    func dropEntered(info: DropInfo) {
-        if let draggedItem {
-            let fromIndex = items.firstIndex(of: draggedItem)
-            if let fromIndex {
-                let toIndex = items.firstIndex(of: destinationItem)
-                if let toIndex, fromIndex != toIndex {
-                    withAnimation {
-                        self.items.move(fromOffsets: IndexSet(integer: fromIndex), toOffset: (toIndex > fromIndex ? (toIndex + 1) : toIndex))
-                    }
-                }
-            }
-        }
-    }
 }
